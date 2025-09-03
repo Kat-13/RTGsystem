752734b7195da5f043389f507f4e418e9839bfdc
Kat-13 <57717453+Kat-13@users.noreply.github.com>
2025-09-03 01:05:23 -0500

    Temporarily comment out index.css import
    Comment out the import statement for index.css temporarily.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'// temp
 import App from './App.jsx'
 import AuthWrapper from './components/auth/AuthWrapper.jsx'
 
